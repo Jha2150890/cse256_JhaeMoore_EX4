fe589da5b83a44cc6e6a3dd91666898edb7a76c4
Jhae Moore <jha2150890@maricopa.edu>
2024-11-11 21:56:04 -0700

    More test cases been added

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -1,5 +1,13 @@
 import pytest
-from guess_the_word import random_func
+from guess_the_word import random_func, correct_func, wrong_func
 
 def test_random_func():
+    assert random_func(3) == "golf"
     assert random_func(1) == "football"
+    assert random_func(5) == "baseball"
+
+def test_correct_func():
+    assert correct_func() == True
+    
+def test_wrong_func():
+    assert wrong_func() == False
